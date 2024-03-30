9cfbe4e1c51fed687b0d5700e21f64a4d2dc761d
mohamed awad <mohamed.awad.elgammal@gmail.com>
2024-03-30 17:14:00 +0200

    add swagger

--- Routes/childRoute.js
+++ Routes/childRoute.js
@@ -1,3 +1,98 @@
+/**
+ * @swagger
+ * paths:
+ *   /child:
+ *     get:
+ *       tags:
+ *         - Children
+ *       summary: Get all children
+ *       produces:
+ *         - application/json
+ *       responses:
+ *         200:
+ *           description: List of all children
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/Child'
+ *         500:
+ *           description: Internal server error
+ *
+ *     post:
+ *       tags:
+ *         - Children
+ *       summary: Create a new child
+ *       consumes:
+ *         - application/json
+ *       produces:
+ *         - application/json
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Child'
+ *       responses:
+ *         200:
+ *           description: New child created
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Child'
+ *         400:
+ *           description: Bad request
+ *         500:
+ *           description: Internal server error
+ *
+ *   /child/{id}:
+ *     get:
+ *       tags:
+ *         - Children
+ *       summary: Get child by ID
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID of the child to retrieve
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         200:
+ *           description: Child found
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Child'
+ *         404:
+ *           description: Child not found
+ *         500:
+ *           description: Internal server error
+ *
+ *     delete:
+ *       tags:
+ *         - Children
+ *       summary: Delete child by ID
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID of the child to delete
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         204:
+ *           description: Child deleted successfully
+ *         404:
+ *           description: Child not found
+ *         500:
+ *           description: Internal server error
+ */
+
+
 const express = require("express");
 const controller = require("../controller/childController");
 const {
@@ -7,12 +102,14 @@ const {
   updateValidator
 } = require("./../middleware/validations/childValidator");
 const { childIdvalidator } =require("./../middleware/validations/childValidator");
+const { isAdmin } = require("./../middleware/authenticationMW");
+const { isTeacher } = require("./../middleware/authenticationMW");
 const validatonResult = require("../middleware/validations/validationResult");
 const router = express.Router();
 
 router
   .route("/child")
-  .get(controller.getAllChildren)
+  .get(isAdmin,controller.getAllChildren)
   .post(
     (req, res, next) => {
       console.log(req.body);
@@ -22,9 +119,9 @@ router
     validatonResult,
     controller.insertChild
   )
-  .patch(updateValidator, validatonResult, controller.updateChild);
+  .patch(updateValidator, validatonResult,isAdmin, controller.updateChild);
 
-router.route("/child/:id").get(childIdvalidator,validatonResult,controller.getChildById).delete(controller.deleteChild);
+router.route("/child/:id").get(childIdvalidator,validatonResult,isAdmin,controller.getChildById).delete(isAdmin,controller.deleteChild);
 
 
 module.exports = router;
--- Routes/classRouter.js
+++ Routes/classRouter.js
@@ -1,5 +1,203 @@
+/**
+ * @swagger
+ * openapi: "3.0.0"
+ * info:
+ *   version: "1.0.0"
+ *   title: Nursery System API
+ *   description: API documentation for Nursery System
+ * basePath: /api/v1
+ * tags:
+ *   - name: Teachers
+ *     description: Operations related to teachers
+ *   - name: Classes
+ *     description: Operations related to classes
+ *   - name: Children
+ *     description: Operations related to children
+ * securitySchemes:
+ *   bearerAuth:
+ *     type: http
+ *     scheme: bearer
+ *     bearerFormat: JWT
+ *
+ * paths:
+ *   /class:
+ *     get:
+ *       tags:
+ *         - Classes
+ *       summary: Get all classes
+ *       security:
+ *         - bearerAuth: []
+ *       produces:
+ *         - application/json
+ *       responses:
+ *         200:
+ *           description: List of all classes
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: array
+ *                 items:
+ *                   $ref: '#/components/schemas/Class'
+ *         500:
+ *           description: Internal server error
+ *
+ *     post:
+ *       tags:
+ *         - Classes
+ *       summary: Create a new class
+ *       security:
+ *         - bearerAuth: []
+ *       consumes:
+ *         - application/json
+ *       produces:
+ *         - application/json
+ *       requestBody:
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Class'
+ *       responses:
+ *         200:
+ *           description: New class created
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Class'
+ *         400:
+ *           description: Bad request
+ *         500:
+ *           description: Internal server error
+ *
+ *   /class/{id}:
+ *     get:
+ *       tags:
+ *         - Classes
+ *       summary: Get class by ID
+ *       security:
+ *         - bearerAuth: []
+ *       produces:
+ *         - application/json
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID of the class to retrieve
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         200:
+ *           description: Class found
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Class'
+ *         404:
+ *           description: Class not found
+ *         500:
+ *           description: Internal server error
+ *     delete:
+ *       tags:
+ *         - Classes
+ *       summary: Delete class by ID
+ *       security:
+ *         - bearerAuth: []
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID of the class to delete
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         204:
+ *           description: Class deleted successfully
+ *         404:
+ *           description: Class not found
+ *         500:
+ *           description: Internal server error
+ *
+ *   /class/child/{id}:
+ *     get:
+ *       tags:
+ *         - Classes
+ *       summary: Get class information for a child by child ID
+ *       security:
+ *         - bearerAuth: []
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID of the child
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         200:
+ *           description: Class information found for the child
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Class'
+ *         404:
+ *           description: Class information not found for the child
+ *         500:
+ *           description: Internal server error
+ *
+ *   /class/teacher/{id}:
+ *     get:
+ *       tags:
+ *         - Classes
+ *       summary: Get class information for a teacher by teacher ID
+ *       security:
+ *         - bearerAuth: []
+ *       parameters:
+ *         - name: id
+ *           in: path
+ *           description: ID of the teacher
+ *           required: true
+ *           schema:
+ *             type: string
+ *       responses:
+ *         200:
+ *           description: Class information found for the teacher
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Class'
+ *         404:
+ *           description: Class information not found for the teacher
+ *         500:
+ *           description: Internal server error
+ *
+ * components:
+ *   schemas:
+ *     Class:
+ *       type: object
+ *       required:
+ *         - _id
+ *         - name
+ *         - supervisor
+ *       properties:
+ *         _id:
+ *           type: number
+ *           description: The ID of the class
+ *         name:
+ *           type: string
+ *           description: The name of the class
+ *         supervisor:
+ *           type: string
+ *           description: The ID of the supervisor teacher
+ *         children:
+ *           type: array
+ *           description: The list of children IDs in the class
+ *           items:
+ *             type: number
+ *           uniqueItems: true
+ */
+
+
 const express = require("express");
-const controller = require("../controller/classController");
+const controller = require("../controller/classController")
 const {
   insertValidator,
   updateValidator,
@@ -7,23 +205,26 @@ const {
   validateTeacherId,
   classIdValidator
 } = require("./../middleware/validations/classValidator");
+const { isAdmin } = require("./../middleware/authenticationMW");
+const { isTeacher } = require("./../middleware/authenticationMW");
 const validatonResult = require("../middleware/validations/validationResult");
 const router = express.Router();
 
 router
   .route("/class")
-  .get(controller.getAllClasses)
+  .get(isAdmin,controller.getAllClasses)
   .post(
     insertValidator,
     validatonResult,
+    isAdmin,
     controller.insertClass
   )
-  .patch(updateValidator, validatonResult, controller.updateClass);
+  .patch(updateValidator, validatonResult,isAdmin, controller.updateClass);
   
 
-router.route("/class/:id").get( classIdValidator, validatonResult,controller.getClassById).delete(controller.deleteClass);
-router.route("/class/child/:id").get( validateChildId, validatonResult, controller.getClassChildrenInfo);
-router.route("/class/teacher/:id").get( validateTeacherId, validatonResult, controller.getClassTeacherInfo);
+router.route("/class/:id").get( classIdValidator, validatonResult,isAdmin,controller.getClassById).delete(controller.deleteClass);
+router.route("/class/child/:id").get( validateChildId, validatonResult,isAdmin, controller.getClassChildrenInfo);
+router.route("/class/teacher/:id").get( validateTeacherId, validatonResult,isAdmin, controller.getClassTeacherInfo);
 
 
 
